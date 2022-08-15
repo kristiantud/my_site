f10ea799d6e71d27c89c8a082bc4ea96e8119720
Kristian Tud <kristiantud@hotmail.com>
2022-08-15 16:41:38 -0600

    removed all the node stuff

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+
 function spotifyAPI() {
     // get spotify recently played
+    // let token = "Bearer " + "";
     let xhr = new XMLHttpRequest();
     xhr.open("GET", "https://api.spotify.com/v1/me/player/recently-played?limit=1&after=1484811043508");
-    xhr.setRequestHeader('Authorization', 'Bearer BQBW6fW40chYlC0U7-bc82afxVer2wCrA0SeivNPolbMYZdWTWGhmL0FuVXuGh1Q8rOmuQpxhE01x2m49ScwWnnrQdo5Ip2Zc8vggIVFBgNg0dlABFro9vV0iZO7LF9OpDipzaMqesPptTGUyTS_ifggWsZmG-B-DUj3d23VSjKIUHuixLU8mkXCUrfGbtXWwK-mhoOs');
+    xhr.setRequestHeader('Authorization', 'Bearer BQBcLgx5YxNnWYiAEBbbDWEM909JVOK0UjyS5C3OvZV5OcJ9vZw7WJcYry4E-vgaQrPilUZPwX8tiZPVZW2cSKwY_1tMSdspxvViHzvAiA_xPk-gk9crXOlvIXMo7mJ-BOv31vim0dg8rXnto94HY1aSOwWcwE6uW1p_2nda2T4gVhqt1G5fL3sV67ZImbWTHrXU60zE');
     xhr.send();
 
     xhr.onload = () => {
@@ -30,3 +32,5 @@ function spotifyAPI() {
         console.log(JSONified.items[0].track.album.images[1].url);
     };
 }
+
+
