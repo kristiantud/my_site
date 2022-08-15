68543b3aebf574de7b50370d979511b8a9d1e736
Kristian Tud <kristiantud@hotmail.com>
2022-08-15 15:30:23 -0600

    finished spotify. TODO: figure out the expiring token conundrum

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function spotifyAPI() {
     // get spotify recently played
     let xhr = new XMLHttpRequest();
     xhr.open("GET", "https://api.spotify.com/v1/me/player/recently-played?limit=1&after=1484811043508");
-    xhr.setRequestHeader('Authorization', 'Bearer BQAPjR05_Bm4fNw1vvxOhRd9sluu6XY19UIsk5_YoNBPMGs5z1WZFtMluQkUtLp4qGRBYvFd59BaS5fYh5J0SuYCs_sL5ClD07LH1PrFf5QVbbHFjvct6cNl7fW6CiNZe3sO8pHyX9AlsYyQ4T8p47pPqBtL2yfNez29f19RWNcv-R8hY-XURdcWNPoBq1ZsFuMIdPWj');
+    xhr.setRequestHeader('Authorization', 'Bearer BQBW6fW40chYlC0U7-bc82afxVer2wCrA0SeivNPolbMYZdWTWGhmL0FuVXuGh1Q8rOmuQpxhE01x2m49ScwWnnrQdo5Ip2Zc8vggIVFBgNg0dlABFro9vV0iZO7LF9OpDipzaMqesPptTGUyTS_ifggWsZmG-B-DUj3d23VSjKIUHuixLU8mkXCUrfGbtXWwK-mhoOs');
     xhr.send();
 
     xhr.onload = () => {
@@ -15,14 +15,18 @@ function spotifyAPI() {
         songname = songname + JSONified.items[0].track.name;
         songartist = songartist + JSONified.items[0].track.artists[0].name;
 
-        console.log(songname);
-        console.log(songartist);
+        // console.log(songname);
+        // console.log(songartist);
 
 
-        const song_name = document.getElementById('#song_title');
-        const song_artist = document.getElementById('#song_artist');
+        const song_name = document.getElementById('song_title');
+        const song_artist = document.getElementById('song_artist');
+        const album_cover = document.getElementById('spotify-album-cover');
 
         song_name.innerText = songname;
         song_artist.innerText = songartist;
+        album_cover.src = JSONified.items[0].track.album.images[1].url;
+
+        console.log(JSONified.items[0].track.album.images[1].url);
     };
 }
