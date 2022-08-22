b1e963847d4dd1b41e67ff0fefc6a4d66a7c86cd
Kristian Tud <kristiantud@hotmail.com>
2022-08-22 06:35:50 -0600

    finished the projects section and the design section

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function spotifyAPI() {
     // get spotify recently played
     let xhr = new XMLHttpRequest();
-    const token = 'BQAffhFLwMhbnEpWG53meNhPGAVUcvFmehV6HpqJwY6MzCXc-ssMN5ltZ5kXS1LX7wG8MaAXUtsLcx_qIRS5WTxPgXu-YgvlQjfBJFcSBnNGxWgLuCWZcNE41ll_RFw5KT7pppNuO4ZtOxhnQWulCSVUFdoHiVKtmzchEZqm3pZqoN_SCCQXOFoV1OWLk_B0oOKZNsjZ';
+    const token = 'BQBmAOpdIacKvaFDqvx-DiI5KotemeG_yC00HtdW_Xi34WoUZFv8cdkSdkXWKjWZP8ETJNPYiFCBuUgPIoRg4opmJ6zERwGBxVYnBb6B5hczW4rbcDf033m06sA_2Qd1X4g7u7M9YAOgjKJUS2VRFuDRVVCEYhlpo-GW25Lc_HzGF9xb0nNvp_pyBFQ27A52mq6UZgs9';
     xhr.open("GET", "https://api.spotify.com/v1/me/player/recently-played?limit=1&after=1484811043508");
     xhr.setRequestHeader('Authorization', 'Bearer ' + token );
     xhr.send();
@@ -65,4 +65,54 @@ function goTo(site){
     if (site==="instagram"){
         window.open("https://instagram.com/ktud__/", '_blank').focus();
     }
+
+    if (site==="adobe_paws"){
+        window.open("https://xd.adobe.com/view/ebd65a8d-834b-4d44-ab10-c1f65cbea44b-be85/", '_blank').focus();
+    }
+
+    if (site==="figma_boardem"){
+        window.open("https://www.figma.com/file/BBtgTziTJjeGUSDt8p1BEb/boardem.?node-id=0%3A1" , '_blank').focus();
+    }
+
+    if (site==="figma_kristiantudv1"){
+        window.open("https://www.figma.com/file/BHAkQMAXdXpt06KN2SepYf/Resume?node-id=0%3A1",'_blank').focus();
+    }
+
+    if (site==="git-todos"){
+        window.open("https://git.cs.usask.ca/krt916/finalproject350",'_blank').focus();
+    }
+
+    if (site==="bionic-reading"){
+        window.open("https://github.com/kristiantud/BionicReading",'_blank').focus();
+    }
+
+    if (site==="complido-cars"){
+        window.open("https://complidocars.com",'_blank').focus();
+    }
+
+    if (site==="discord-bot-warzone"){
+        window.open("https://github.com/kristiantud/mw_discord_bot",'_blank').focus();
+    } 
+
+    if (site==="boardem-site"){
+        window.open("https://kristiantud.github.io/boardem/",'_blank').focus();
+    }
+
+    if (site==="rock-paper-scissors"){
+        window.open("https://git.cs.usask.ca/krt916/networking306",'_blank').focus();
+        
+    }
+
+    if (site==="git-hanabi"){
+        window.open("https://git.cs.usask.ca/370-19/h1",'_blank').focus();
+    }
+
+    if (site==="git-chatrooms"){
+        window.open("https://git.cs.usask.ca/krt916/assignment4_350/",'_blank').focus();
+    }
+
+    
+
+    
+    
 }
